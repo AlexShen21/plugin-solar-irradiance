701201e3757e967e83afea787b8dfd42d3b5303e
AlexShen21 <121585614+AlexShen21@users.noreply.github.com>
2023-08-01 14:53:46 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -12,17 +12,14 @@ from PIL import Image
 import argparse
 
 
-#get_args taken from surface water classifier
-
-
-
+#get_args function taken from surface water classifier
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-model', action = 'store', 
                         dest='model', default='model.pt', 
                         help='path to model')
     parser.add_argument('-stream', dest='stream',
-                action='store', default="left_camera",
+                action='store', default="top_camera",
                 help='ID or name of a stream, e.g. sample')
     parser.add_argument(
         '-debug', dest='debug',
@@ -36,8 +33,13 @@ def get_args():
 
 
 def run(model, sample, plugin):
+    #Take the image sample from camera
     image =sample.data
+
+    #Get the timestamp 
     timestamp = sample.timestamp
+
+    #Transform to PIL Image then resize to the size of images that model was trained on
     transformation = transforms.Compose([transforms.ToPILImage(),
                                          transforms.Resize((224,224)),
                                          transforms.ToTensor(),
@@ -46,39 +48,39 @@ def run(model, sample, plugin):
     image = transformation(image)
 
 
+    
+    #Use either Cuda or CPU
     image = image.to(args.device).unsqueeze(0)
 
 
-
+    #Perform Inference
     with torch.no_grad():
     	output = model(image)
 
 
 
-
-    #inference
-
+    #Store inference
     predicted_irradiance = output.item()
 
+    #Print the predicted irradiance to logs
     print(f"Current Solar Irradiance: {predicted_irradiance}")
 
-
-
+    #Publish the estimated irradiance data
     plugin.publish('env.solar.irradiance', predicted_irradiance, timestamp = timestamp)
 
 
 
 if __name__ == "__main__":
+    
     args = get_args()
+
+    #Set the device
     if torch.cuda.is_available():
         args.device = 'cuda:0'
     else:
         args.device = 'cpu'
-
-    # model_dict = torch.load(args.model, map_location= args.device)
-    # model = torchvision.models.load_state_dict(model_dict)
-    # model.eval()
-
+    
+    #Load saved model from LCRC
     model = torchvision.models.resnet50()
     num_features = model.fc.in_features
     model.fc = torch.nn.Linear(num_features, 1)
@@ -88,7 +90,8 @@ if __name__ == "__main__":
     model = model.to(args.device)
 
     model.eval()
-    print("Hello, This is a test Run")
+
+    #Use Camera to get image and run it through the above functions
     while True:
         with Plugin() as plugin, Camera(args.stream) as camera:
             sample = camera.snapshot()
